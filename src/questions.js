e811751010eb9f3794167277d334b3b5aba27e03
Sebastian Portesi <sportesi@octopus.com.ar>
2019-11-06 12:10:09 -0300

    preguntas

--- src/questions.js
+++ src/questions.js
@@ -1,23 +1,523 @@
 const questions = {
     questions: [
         {
-            i: 1,
-            q: "¿cuales de los siguientes libros son obligatorios para tus consorcios?",
-            o: [
-                {n: "a", a: "libro de actas", c: false},
-                {n: "b", a: "libro de propietarios", c: false},
-                {n: "c", a: "libro de sueldos", c: false},
-                {n: "d", a: "todos", c: true},
+            "i": 1,
+            "q": "¿ A que se refiere la ley 25.326 ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Permisos para  ̈Circuitos de Televisión",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Protección de Datos Personales",
+                    "c": true
+                },
+                {
+                    "n": "C",
+                    "a": "Obligaciones del Administrador",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Protección contra incendios,",
+                    "c": false
+                }
             ]
         },
         {
-            i: 2,
-            q: "¿quien es el mas groso de octopus?",
-            o: [
-                {n: "a", a: "seba", c: true},
-                {n: "b", a: "chiama", c: false},
-                {n: "c", a: "bacci", c: false},
-                {n: "d", a: "guido", c: false},
+            "i": 2,
+            "q": "¿ Que libro es obligatorio llevar a una Asamblea junto con el Libro\nde Actas ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Libro de Propietarios",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Libro de Administración o Caja",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Libro de Registro de Firmas",
+                    "c": true
+                },
+                {
+                    "n": "D",
+                    "a": "Libro de Sueldos",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 3,
+            "q": "¿ Cuál es el organismo que controla el cumplimiento de la Ley 257 ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Subsecretaria de trabajo, industria y comercio ( STIC )",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Agencia Gubernamental de control ( AGC)",
+                    "c": true
+                },
+                {
+                    "n": "C",
+                    "a": "Registro Publico de Administradores de Consorcios ( RPA )",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Administración de Ingresos Públicos",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 4,
+            "q": "¿ Cada cuanto debe reemplazarse el polvo de un matafuego Tipo ABC ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Cada 10 años",
+                    "c": true
+                },
+                {
+                    "n": "B",
+                    "a": "Cada 7 años",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Cada 5 años",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Cuando se haya agotado",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 5,
+            "q": "¿ Cuáles son los libros laborales obligatorios para un consorcio ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Libro de registro de entradas y salidas del personal.",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Libro de registro de firmas y libro de propietarios",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Libro de Sueldos, Órdenes y Horas Extras",
+                    "c": true
+                },
+                {
+                    "n": "D",
+                    "a": "Libro de administracion y libro de actas.",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 6,
+            "q": "¿ Cuál es el plazo de validez de un certificado de Medición de Puesta a Tierra ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "1 año",
+                    "c": true
+                },
+                {
+                    "n": "B",
+                    "a": "2 años",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "3 años",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "No vence",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 7,
+            "q": "¿ Cual es el organismo responsable de la Declaración Jurada Anual de los consorcios ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Direccion General de Empleo",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Dirección General del Defensa y Protección al Consumidor",
+                    "c": true
+                },
+                {
+                    "n": "C",
+                    "a": "Agencia Gubernamental de Control",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Subsecretaria de trabajo, industria y comercio.",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 8,
+            "q": "¿ Cuál es la vida útil de un matafuegos CO2 ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "10 años",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "15 años",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "30 años",
+                    "c": true
+                },
+                {
+                    "n": "D",
+                    "a": "No vecen , deben recargarse luego de ser utilizados.",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 9,
+            "q": "¿ Que requisito se implementó este año para poder renovar la matrícula del administrador ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Curso de separación de residuos",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Detalle de estado de deuda de AFIP",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Certificado libre deuda expedido Registro de Deudores/as Alimentarios/as Morosos/as.",
+                    "c": true
+                },
+                {
+                    "n": "D",
+                    "a": "Certificado del Registro Nacional de Reincidencia y Estadística Criminal",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 10,
+            "q": "¿ Cual de las siguientes instalaciones lleva Oblea QR de control ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Elevadores",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Instalaciones Fijas Contra Incendios",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Instalaciones Termicas",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Todas las respuestas son correctas",
+                    "c": true
+                }
+            ]
+        },
+        {
+            "i": 11,
+            "q": "Las siglas AGC, que corresponden a la entidad que regula el mantenimiento de instalaciones de un edificio, significa:",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Agencia General de la Ciudad",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Agencia Gubernamental de la Ciudad",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Agencia General de Control",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Agencia Gubernamental de Control",
+                    "c": true
+                }
+            ]
+        },
+        {
+            "i": 12,
+            "q": "Si administro un consorcio radicado en CABA que comenzó su actividad de empleador este año, ¿ cuál de los siguientes trámites NO debo realizar este año ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Registro de Empleadores",
+                    "c": true
+                },
+                {
+                    "n": "B",
+                    "a": "Declaración Formulario 931 de AFIP",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Declaración Jurada Anual del consorcio",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "B y C son correctas.",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 13,
+            "q": "Para ser administrador AD HONOREM (Voluntario), es necesario:",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Estar inscripto y al dia en el RPA",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Adminsitrar algun otro consorcio",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Residir en una UF ( unidad funcional) del edificio en cuestión",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "A y C son correctas",
+                    "c": true
+                }
+            ]
+        },
+        {
+            "i": 14,
+            "q": "En relación a la declaración jurada anual de consorcios: ¿que consorcios debo declarar este año?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Todos los consorcios que administro actualmente sin importar cuándo fue mi designación.",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Todos los consorcios que administre total o parcialmente durante el año calendario anterior.",
+                    "c": true
+                },
+                {
+                    "n": "C",
+                    "a": "Solo los consorcios que administre durante los 12 meses del año anterior.",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Solo los consorcios del año en curso (2019)",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 15,
+            "q": "¿Que libros obligatorios se rubrican actualmente en el Registro Público de Administradores?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Libro de firmas y propietarios",
+                    "c": true
+                },
+                {
+                    "n": "B",
+                    "a": "Libro de datos 2019, libro de firmas y libro de propietarios",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Libro de Actas",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Libro de Administración",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 16,
+            "q": "El curso de basura cero, requisito necesario para la renovación de la matricula del administrador, ¿ cada cuanto tiempo debe realizarse?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Cada 5 años",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "Es de caracter opcional",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Por única vez",
+                    "c": true
+                },
+                {
+                    "n": "D",
+                    "a": "Una vez por año",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 17,
+            "q": "¿ Cual es la Ley de La Propiedad Horizontal ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Ley 13.512",
+                    "c": true
+                },
+                {
+                    "n": "B",
+                    "a": "Ley 20.744",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Ley 17.418",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Ninguna es correcta",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 18,
+            "q": "El libro de firmas, ¿se reemplazó con el libro de propietarios (homologado en 2017) ?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Sì, el libro de propietarios reemplazò al libro de firmas",
+                    "c": false
+                },
+                {
+                    "n": "B",
+                    "a": "No, ambos libros ya no son obligatorios",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Se pueden tener cualquiera de los dos",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "No, ambos son vigentes y obligatorios",
+                    "c": true
+                }
+            ]
+        },
+        {
+            "i": 19,
+            "q": "¿ Cuál de los siguientes seguros NO es obligatorio para un consorcio que cuenta con empleados?",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "Seguro contra robos y hurtos",
+                    "c": true
+                },
+                {
+                    "n": "B",
+                    "a": "Seguro integral de consorcios",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "Seguro de vida",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "Seguro de ART",
+                    "c": false
+                }
+            ]
+        },
+        {
+            "i": 20,
+            "q": "Cual de las siguientes afirmaciones es correcta.",
+            "o": [
+                {
+                    "n": "A",
+                    "a": "El registro de empleadores se carga de manera online en la pàgina de AGIP",
+                    "c": true
+                },
+                {
+                    "n": "B",
+                    "a": "El registro de empleadores se carga de manera online en la pàgina de AFIP",
+                    "c": false
+                },
+                {
+                    "n": "C",
+                    "a": "El registro de empleadores informa anualmente los sueldos percibidos por los empleados del consorcio",
+                    "c": false
+                },
+                {
+                    "n": "D",
+                    "a": "El registro de empleadores vence el 31 de diciembre de cada año",
+                    "c": false
+                }
             ]
         }
     ],
